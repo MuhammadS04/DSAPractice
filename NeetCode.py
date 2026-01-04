75858f1d44552ab93623acc65c139a68e9d220ba
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2026-01-04 15:24:39 -0800

    Anagram HashMap

--- NeetCode.py
+++ NeetCode.py
@@ -31,15 +31,104 @@
 # now iterate to the next position and repeat this for i and the i + 1 character 
 # store that value into a result int variable and print it 
 
-# what is the optimal solution though ?
 
-#brute force implementation 
+# class Solution:
+#     def scoreOfString(self, s: str) -> int:
+#         sScore = 0
+#         for i in range(len(s) - 1):
+#            char = s[i]
+#            char2 = s[i+1]
+#            sScore += (abs(ord(char2)-ord(char)))
+
+#         return sScore
+    
+
+
+# ===============================================
+# Contains Duplicate
+# ===============================================
+
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+
+# Output: false
+
+#brute force: O(n^2) time complexity)
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         for i in range(len(nums)):
+#             for j in range(i + 1,(len(nums))):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+    
+    #optimal solution:
+
+# Sorted solution (O(n log n) time complexity due to sorting) 
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         for i in range(1, len(nums)):
+#             if nums[i] == nums[i - 1]:
+#                 return True
+#         return False
+
+# Hash Set Optimal Solution (O(n) time complexity)
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         seen = set()
+#         for i in nums:
+#             if i in seen: 
+#                 return True
+#             seen.add(i)
+#         return False
+
+# ===============================================
+# Valid Anagram
+# ==============================================
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: s = "racecar", t = "carrace"
+
+# Output: true
+# Example 2:
+
+# Input: s = "jar", t = "jam"
+
+# Output: false
+# Constraints:
+
+# s and t consist of lowercase English letters.
 class Solution:
-    def scoreOfString(self, s: str) -> int:
-        sScore = 0
-        for i in range(len(s) - 1):
-           char = s[i]
-           char2 = s[i+1]
-           sScore += (abs(ord(char2)-ord(char)))
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countedS = {}
+        countedT = {}
+        
+        for i in range(len(s)):
+            countedS[s[i]] = 1 + countedS.get(s[i],0)
+            countedT[t[i]] = 1 + countedT.get(t[i],0)
+        return countedS == countedT
 
-        return sScore
+
+    
+if __name__ == "__main__":
+    s = "racecar"
+    t = "carrace"
+    sol = Solution()
+    print(sol.isAnagram(s,t))
+    exit
